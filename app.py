0f73c31e9949fea7441cc0989ea6e755073bf7d9
AtharvaMutsaddi <atharvaam21.comp@coep.ac.in>
2024-04-12 07:27:53 +0530

    2 zips

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def within():
     if request.method=="GET":
         return render_template('withinzip.html')
     else:
-        print("Post")
+        # print("Post")
         file = request.files['file']
         filename = file.filename
     
@@ -108,49 +108,35 @@ def within():
             
             superans[ftype]=ans
         
-        print(superans)
+        # print(superans)
         return superans       
 @app.route("/local",methods=["GET","POST"])
 def local():
     if request.method=="GET":
         return render_template('localzips.html')
     else:
-        print("Post")
-        file = request.files['file']
-        filename = file.filename
-    
-        file_path = os.path.join("../uploads", filename)
-        file.save(file_path)
-
-        if filename.endswith(".zip"):
-            filename=filename.split(".")[0]
-            
-        extract_zip_recursively(file_path, "../uploads/")
-
-        folder_structure = get_detailed_report_of_files(f"../uploads/{filename}")
-        print(folder_structure)
-        fmap = get_file_mapping(folder_structure)
-        print(fmap)
-        superans={}
-        for ftype in fmap.keys():
-            rel_file_paths = fmap[ftype]
-            # assuming code for all
-            ans=[]
-            for i in range(len(rel_file_paths)):
-                file_content1=File_Reader().get_type_of_file_and_data(rel_file_paths[i])["file_data"]
-                for j in range(i+1,len(rel_file_paths)):
-                    file_content2=File_Reader().get_type_of_file_and_data(rel_file_paths[j])["file_data"]
-                    subarr=[]
-                    simi=simhash_simi(file_content1,file_content2)
-                    subarr.append(simi)
-                    subarr.append(rel_file_paths[i])
-                    subarr.append(rel_file_paths[j])
-                    ans.append(subarr)
-            
-            superans[ftype]=ans
+        file1 = request.files['file1']
+        filename1 = file1.filename
+        file2 = request.files['file2']
+        filename2 = file2.filename
         
-        print(superans)
-        return superans        
+        if not os.path.exists("../uploads/1"):
+            os.makedirs("../uploads/1")
+        file_path1 = os.path.join("../uploads/1", filename1)
+        file1.save(file_path1) 
+        
+        if not os.path.exists("../uploads/2"):
+            os.makedirs("../uploads/2")
+        file_path2 = os.path.join("../uploads/2", filename2)
+        file2.save(file_path2) 
+        if filename1.endswith(".zip"):
+            filename1=filename1.split(".")[0] 
+        # extract_zip_recursively(file_path1, f"../uploads/1/{file}") 
+        extract_zip_recursively(file_path2, "../uploads/2/") 
+        folder_structure1 = get_detailed_report_of_files(f"../uploads/1/{filename1}")
+        fmap = get_file_mapping(folder_structure1) 
+        return {}
+                
 if __name__ == '__main__':
     extra_dirs = ['uploads']
     app.run(debug=True, extra_files=extra_dirs)
