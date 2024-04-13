d94340218b43ce7c6bdf8d712257770cc759255f
Aditya Choudhary <124642674+aditya-choudhary599@users.noreply.github.com>
2024-04-13 06:03:03 +0530

    Similarity table added

--- chunk_similarity.py
+++ chunk_similarity.py
@@ -2,21 +2,21 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 import Levenshtein
 
 
-def get_similar_chunks(text_1: str, text_2: str) -> list:
-    def split_text_into_chunks(text: str) -> list:
-        return RecursiveCharacterTextSplitter(
-            chunk_size=80, chunk_overlap=10, length_function=len
-        ).split_text(text)
+def split_text_into_chunks(text: str) -> list:
+    return RecursiveCharacterTextSplitter(
+        chunk_size=80, chunk_overlap=0, length_function=len
+    ).split_text(text)
 
+
+def get_similar_chunks(text_1: str, text_2: str) -> list:
+    all_chunks_similarity_mapping = []
     chunks_1 = split_text_into_chunks(text_1)
     chunks_2 = split_text_into_chunks(text_2)
-
-    all_chunks_similarity_mapping = []
 
     for chunk_1 in chunks_1:
         for chunk_2 in chunks_2:
             curr_ratio = Levenshtein.ratio(chunk_1, chunk_2)
-            if curr_ratio >= 0.5:
+            if curr_ratio >= 0.6:
                 all_chunks_similarity_mapping.append([curr_ratio, chunk_1, chunk_2])
 
     # Sorting the mapping in descending order wrt the similarity ratio
@@ -25,25 +25,15 @@ def get_similar_chunks(text_1: str, text_2: str) -> list:
     )
 
     covered_chunks = []
-    ans = []
+    ans = {}
 
     for item in all_chunks_similarity_mapping:
         chunk_1, chunk_2 = item[1::1]
 
         if chunk_1 not in covered_chunks and chunk_2 not in covered_chunks:
-            ans.append(
-                [
-                    chunk_1,
-                    text_1.find(chunk_1),
-                    text_1.find(chunk_1) + len(chunk_1),
-                    chunk_2,
-                    text_2.find(chunk_2),
-                    text_2.find(chunk_2) + len(chunk_2),
-                ]
-            )
+            ans[chunk_1] = chunk_2
             covered_chunks.append(chunk_1)
             covered_chunks.append(chunk_2)
-            # print([chunk_1, chunk_2], end="\n\n")
 
     return ans
 
